88e04b49a0329a40a6d0d077e7b5c376ad3886fc
danielostos21 <danielostos21@gmail.com>
2023-09-02 12:42:27 -0400

    Script for chat function

--- aichat/aichat_app/views.py
+++ aichat/aichat_app/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import openai
+
+openai_api_key = "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"
+openai.api_key = openai_api_key
+
+# def ask_openai(message):
+#     response = openai.ChatCompletion.create(
+#         model = "text-davinci-003",
+#         prommt = message,
+#         n = 1,
+#         stop = None,
+#         max_tokens = 150,
+#         temperature = 0.7,
+#     )   
+#     print(response)
+#     answer = response.choices[0].text.strip();
+#     return answer
 
 # Create your views here.
 def aichat(request):
+    if request.method == 'POST':
+        message = request.POST.get('message')
+       
+        response = "Hola"
+        return JsonResponse({ 'message': message, 'response': response})
+    
     return render(request, 'chatbot.html')
