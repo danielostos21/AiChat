d8a72318c6dd4bad6eb759073acf4de45867098f
danielostos21 <danielostos21@gmail.com>
2023-09-02 13:22:14 -0400

    Implement OpenAi API

--- aichat/aichat_app/views.py
+++ aichat/aichat_app/views.py
@@ -2,28 +2,28 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import openai
 
-openai_api_key = "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"
+openai_api_key = "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"
 openai.api_key = openai_api_key
 
-# def ask_openai(message):
-#     response = openai.ChatCompletion.create(
-#         model = "text-davinci-003",
-#         prommt = message,
-#         n = 1,
-#         stop = None,
-#         max_tokens = 150,
-#         temperature = 0.7,
-#     )   
-#     print(response)
-#     answer = response.choices[0].text.strip();
-#     return answer
+def ask_openai(message):
+    response = openai.Completion.create(
+        model = "text-davinci-003",
+        prompt = message,
+        n = 1,
+        stop = None,
+        max_tokens = 500,
+        temperature = 0.7,
+    )   
+    answer = response.choices[0].text.strip();
+    return answer
+
 
 # Create your views here.
 def aichat(request):
     if request.method == 'POST':
         message = request.POST.get('message')
-       
-        response = "Hola"
+        
+        response = ask_openai(message)
         return JsonResponse({ 'message': message, 'response': response})
     
     return render(request, 'chatbot.html')
