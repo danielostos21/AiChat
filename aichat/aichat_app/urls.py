d83e94ed2a875cf05ad5059740b10607955f18ff
danielostos21 <danielostos21@gmail.com>
2023-09-03 08:57:00 -0400

    login futionalities

--- aichat/aichat_app/urls.py
+++ aichat/aichat_app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.aichat, name='aichat')
+    path('', views.aichat, name='aichat'),
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+    path('logout', views.logout, name='logout'),
 ]
--- aichat/aichat_app/views.py
+++ aichat/aichat_app/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
+from django.contrib import auth
 import openai
+
+
 
 openai_api_key = "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"
 openai.api_key = openai_api_key
@@ -27,3 +30,33 @@ def aichat(request):
         return JsonResponse({ 'message': message, 'response': response})
     
     return render(request, 'chatbot.html')
+
+
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST ['username']
+        email = request.POST ['email']
+        password1 = request.POST ['password1']
+        password2 = request.POST ['password2']
+        if password1 == password2:
+            try:
+                user = auth.models.User.objects.create_user(username, email, password1)
+                user.save()
+                auth.login(request, user)
+                return (redirect('chatbot'))
+            except:
+                error_message = 'Error creating account'
+                    
+        else:
+            error_message = 'password dont match'
+            return render(request, 'register.html', {'error_message': error_message})
+
+    
+    return render(request, 'register.html')
+
+def logout(request):
+    auth.logout(request)
