a43a3edaafe9e0a8b5aabbd16636c93163f8638c
danielostos21 <danielostos21@gmail.com>
2023-09-04 03:38:41 -0400

    migrate to MySQL

--- aichat/aichat/settings.py
+++ aichat/aichat/settings.py
@@ -76,11 +76,15 @@ WSGI_APPLICATION = 'aichat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'username$database_name',
+            'USER': 'username',
+            'PASSWORD': 'mysql_password',
+            'HOST': 'mysql_hostname',
+}
 }
 
 
