c217412a4d5852732eca4fbbe08b6fb582f97eac
danielostos21 <danielostos21@gmail.com>
2023-09-03 23:22:01 -0400

    New home. updated views

--- aichat/aichat_app/views.py
+++ aichat/aichat_app/views.py
@@ -2,35 +2,52 @@ from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.contrib import auth
 from django.contrib.auth.models import User
+from .models import Chat
+from django.utils import timezone
 import openai
 
 
 
-openai_api_key = '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'
+openai_api_key = 'input your'
 openai.api_key = openai_api_key
 
 def ask_openai(message):
-    response = openai.Completion.create(
-        model = "text-davinci-003",
-        prompt = message,
-        n = 1,
-        stop = None,
-        max_tokens = 500,
-        temperature = 0.7,
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": message}
+        ]
     )   
-    answer = response.choices[0].text.strip();
+    
+    answer = response.choices[0].message.content.strip()
     return answer
 
 
 # Create your views here.
 def aichat(request):
-    if request.method == 'POST':
-        message = request.POST.get('message')
-        
-        response = ask_openai(message)
-        return JsonResponse({ 'message': message, 'response': response})
     
-    return render(request, 'chatbot.html')
+
+    if request.user.is_authenticated:
+        chats = Chat.objects.filter(user=request.user)
+        if request.method == 'POST':
+            
+            message = request.POST.get('message')     
+            response = ask_openai(message)
+            chat = Chat(user=request.user, message= message, response=response, created_at=timezone.now)
+            chat.save()
+            return JsonResponse({ 'message': message, 'response': response})
+    
+    else:
+        error_message = 'You need to login to start chat'
+        return redirect('login', {'error_message': error_message})
+
+   
+
+    
+
+    
+    return render(request, 'chatbot.html', {'chats': chats})
 
 
 
@@ -75,3 +92,12 @@ def register(request):
 def logout(request):
     auth.logout(request)
     return redirect('login')
+
+
+def home(request):
+    return render(request, 'home.html')
+
+   
+   
+def portafolio(request):
+    return redirect('http://danielostos21.github.io/public_html/index.html')
